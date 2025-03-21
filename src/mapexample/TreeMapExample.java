09b6fd325730e5342096b31d875b135fa86dc577
Vikas Upadhyay <vikasu899@gmail.com>
2025-03-21 11:47:34 +0530

    collection examples

--- src/mapexample/TreeMapExample.java
+++ src/mapexample/TreeMapExample.java
@@ -2,7 +2,6 @@ package mapexample;
 
 import java.util.Map;
 import java.util.TreeMap;
-
 public class TreeMapExample {
     public static void main(String[] args) {
         Map<Integer, String> map1=new TreeMap<>();
